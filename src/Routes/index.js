9a562590bb4e8a16ae07fa9918d57843798e6b76
joviabebeto <brigittetuyishimire5@gmail.com>
2023-09-28 10:40:31 +0200

    my product

--- src/Routes/index.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import  Router  from "./userRoutes";
-const router=express.Router();
-router.use("/user",userRoutes)
-export default router
--- src/Routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import userController from "../controller/userController";
-import Datachequer from "../middlewares/datachequer";
-import  Validator  from "../middlewares/Validator";
-
-const router=express.Router();
